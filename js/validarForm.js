7336cc1a18ad557a58b91a0d7810774d38989e8b
Jasernagi <jasernagi24@gmail.com>
2020-02-18 18:24:16 -0500

    subir primeros archivos

---
+++ js/validarForm.js
@@ -0,0 +1,8 @@
+var mensajes=["El nombre no debe contener numeros ni caracteres especiales","El apellido no debe contener numeros ni caracteres especiales",
+"Debe ingresar un correo electronico valido","Debe ingresar su ciudad de residencia", "Debe ingresar su direccion correcta",
+"Debe ingresar solo su numero de celular", "Contraseña solo alfanumerica con minimo 8 digitos"];
+
+function validar(id){
+    var elemento = document.getElementById(id);
+
+}
